f46643bb16362c20106d5400c1b9103d0814df8e
yudhaputraa <elekronikayudha@gmail.com>
2022-03-26 20:37:53 +0700

    belajar continue dan pass

---
+++ py/continue_pass.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+    print('nilai saat ini adalah ', i)
+    if i == 6:
+        print("nilai i adalah",i)
+        # break : fungsinya utk mengakhiri for (terminasi)
+        # continue : fungsinya utk melanjutkan ke step berikutnya
+        # pass : tdk pengaruh apa apa
+        print("cek bro !")
+else:
+    print('akhir dari loop/for')
+print("print ini di luar loop/for")
