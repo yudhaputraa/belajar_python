fc2667b6fef79fefa4f0f49b18ea93b370331790
yudhaputraa <elekronikayudha@gmail.com>
2022-04-08 14:15:35 +0700

    belajar import

---
+++ py/import.py
@@ -0,0 +1,19 @@
+#import matematika
+
+#matematika.tambah(3,3)
+#matematika.kurang(3,3)
+
+#import matematika as m
+
+#m.tambah(3,4)
+#m.kurang(3.4)
+
+#from matematika import tambah,kurang
+#from matematika import *
+
+#tambah(4,5)
+#kurang(5,4)
+
+from matematika import tambah as t
+
+t(5,4)
---
+++ py/matematika.py
@@ -0,0 +1,8 @@
+def tambah(a,b):
+    hasil=a+b
+    print(a,'+',b,'=',hasil)
+
+
+def kurang(a, b):
+    hasil = a+b
+    print(a, '+', b, '=', hasil)
