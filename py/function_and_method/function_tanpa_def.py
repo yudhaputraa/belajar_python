8323529a02ca52db6ee9555361cc3327020812b3
yudhaputraa <elekronikayudha@gmail.com>
2022-04-11 12:48:02 +0700

    function tanpa def

---
+++ py/function_and_method/function_tanpa_def.py
@@ -0,0 +1,9 @@
+pangkat = lambda a,b: a ** b
+
+test1 = pangkat(10,2)
+print(test1)
+
+"""
+membuat funtion yang tidak memiliki nama[sering disebut anonymous functon]
+yang hanya digunakan untuk menyelesaikan perhitungan sederhana
+"""
