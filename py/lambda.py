c9496aa0dd38945ec90f08b92585d1115cb9a426
yudhaputraa <elekronikayudha@gmail.com>
2022-04-11 09:53:56 +0700

    membuat anonymous function dengan lambda

---
+++ py/lambda.py
@@ -0,0 +1,14 @@
+def jumlah(a,b):
+    c = a+b
+    return c
+
+hasil = jumlah(4,5)
+print(hasil)
+
+print('='*15)
+
+# membuat anonymous function dengan lambda
+
+kali = lambda x,y: x*y
+hasil = kali(3,4)
+print(hasil)
