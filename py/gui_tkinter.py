f302fdb7ff559335e4400ec8de230cdf97899ce3
yudhaputraa <elekronikayudha@gmail.com>
2022-04-11 09:55:18 +0700

    belajar gui dgn tkinter

---
+++ py/gui_tkinter.py
@@ -0,0 +1,14 @@
+import tkinter
+
+main_window = tkinter.Tk()
+
+def event_tekan():
+    label2 = tkinter.Label(main_window, text="aku ditekan ^_^")
+    label2.pack()
+
+label = tkinter.Label(main_window, text="halo, saya adalah \n GUI sederhana \n menggunakan python")
+tombol = tkinter.Button(main_window, text="tekan akuh", command= event_tekan)
+
+label.pack()
+tombol.pack()
+main_window.mainloop()
