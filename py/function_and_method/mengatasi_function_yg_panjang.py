c4eb109426d0928ddf261ed65c2edb390bdbd016
yudhaputraa <elekronikayudha@gmail.com>
2022-04-12 12:39:23 +0700

    mengatasi funtion yg panjang menjadi pendek

---
+++ py/function_and_method/mengatasi_function_yg_panjang.py
@@ -0,0 +1,10 @@
+def contoh_nama_fungsi_yang_sangat_panjang(lst):
+    for i in lst:
+        print(i, end=' ')
+
+# contoh manggil fungsi yg repot
+contoh_nama_fungsi_yang_sangat_panjang(['python','ruby','java'])
+
+#cara mempermudah
+tulis = contoh_nama_fungsi_yang_sangat_panjang
+tulis(['python','ruby','java','php','javascript'])
