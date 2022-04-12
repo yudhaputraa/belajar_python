fb75c60891f77d3d0dbf893c63ec6fecbc6532dc
yudhaputraa <elekronikayudha@gmail.com>
2022-04-12 14:33:26 +0700

    mengatasi parameter banyak nilai

---
+++ py/function_and_method/satu_parameter_banyak_nilai.py
@@ -0,0 +1,10 @@
+def test(*nilai):
+    print(nilai)
+
+test(1,2,34,3,45)
+test(234,456,3243)
+
+"""
+dengan menyerakan tanda asterisk satu kali di depan nama parameter. 
+maka parameter tersebut akan berperan sebagai objek tuple
+"""
