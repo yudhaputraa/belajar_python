f3bbd6d513f6bfd3e8f96c6e39e5ea335cffc138
yudhaputraa <elekronikayudha@gmail.com>
2022-04-24 18:23:26 +0700

    membuat write

---
+++ py/direktori/membuat_write.py
@@ -0,0 +1,12 @@
+# membuat file
+f = open('C:\python\direktori\data\data.txt', 'w')
+
+# penulisan data ke file
+f.write('baris pertama \n')
+f.write('baris kedua \n')
+f.write('baris ketiga \n')
+f.write('baris keempat \n')
+f.write('baris kelima \n')
+
+# penutup
+f.close()
