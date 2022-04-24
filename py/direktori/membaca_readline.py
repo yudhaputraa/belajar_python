30b6f3e5e89e347467d89266b686209098e60922
yudhaputraa <elekronikayudha@gmail.com>
2022-04-24 18:27:19 +0700

    membaca readline

---
+++ py/direktori/membaca_readline.py
@@ -0,0 +1,12 @@
+# membuka file
+f = open('C:\python\direktori\data\data.txt','r')
+
+# membaca per karakter mengunakan while
+karakter = f.readline()
+print(karakter)
+#while karakter:
+#    print(karakter, end=' ')
+#    karakter = f.readline()
+
+# menutup file
+#f.close()
