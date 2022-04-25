e90b466a8f00b46653f915ab1ed3f2972271b599
yudhaputraa <elekronikayudha@gmail.com>
2022-04-25 21:05:53 +0700

    memeriksa keberadan file

---
+++ py/direktori/memeriksa_keberadaan2.py
@@ -0,0 +1,8 @@
+import os
+
+# memeriksa keberadaan direktori
+a = os.path.isdir('C:\python')
+print(a)
+# memeriksa keberadaan direktori
+d = os.path.isfile('C:\python\direktori\data\data.txt')
+print(d)
