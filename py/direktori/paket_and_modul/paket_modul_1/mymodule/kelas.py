687e93419458c0b770d1c08f9cc6363817089a1c
yudhaputraa <elekronikayudha@gmail.com>
2022-05-15 17:11:49 +0700

    klass

---
+++ py/direktori/paket_and_modul/paket_modul_1/mymodule/kelas.py
@@ -0,0 +1,11 @@
+class c1:
+    def __init__(self):
+        pass
+    def m(self):
+        print('Metode c1.m() dipanggil')
+        
+class c2:
+    def __init__(self):
+        pass
+    def m(self):
+        print('Metode c2.m() dipanggil')
