daa35a69910383e80b10ac0386bbd1dd3b088665
yudhaputraa <elekronikayudha@gmail.com>
2022-05-22 19:49:57 +0700

    hello

---
+++ uji_coba/latihan/hello.py
@@ -0,0 +1,10 @@
+print("Hello word1")
+print("Hello word2")
+print("Hello word3")
+
+print("halo cantikkk")
+
+print("INI adalah percoban compali dengan python")
+
+a = input("apakah berhasil : ")
+print(a)
