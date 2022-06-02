92d0ba29b26032271b7033671d6e1175481349be
yudhaputraa <elekronikayudha@gmail.com>
2022-06-02 10:16:47 +0700

    format string

---
+++ uji_coba/latihan/b1/format_string.py
@@ -0,0 +1,6 @@
+#format string
+# format string menggunakan operasi '%' dan method format
+print '%s pergi ke %s' %('ibu','pasar')
+print '{0}' pergi ke '{1}'.format('ibu','pasar')
+print 'jumlah total : %10.3f' % 10.3333
+print 'jumlah total : {0:10.3f}'.format(10.3333)
